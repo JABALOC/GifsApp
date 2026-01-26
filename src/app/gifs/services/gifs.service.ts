4782a556278878d644161d88e1502c40897b8f20
Jorge Abalo <jabalo1988@gmail.com>
2026-01-26 12:49:20 +0100

    Curso de angular

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { inject, Injectable } from "@angular/core";
 import { map, Observable } from "rxjs";
-import { KlipyResponse } from "../interfaces/kiply.interfaces";
+import type { KlipyResponse } from "../interfaces/kiply.interfaces";
 import { environment } from "@environments/environment.development";
 
 @Injectable({ providedIn: 'root' })
@@ -10,10 +10,13 @@ export class GifService {
   private http = inject(HttpClient);
 
   loadTrendingGifs(): Observable<string[]> {
+
     return this.http.get<KlipyResponse>(environment.klipyUrl, {
-      headers: {
-        'X-KLIPY-API-KEY': environment.klipyApiKey
-      }
+      // headers: {
+      //   'X-KLIPY-API-KEY': environment.klipyApiKey
+      // }
+
+
     }).pipe(
       map(response =>
         response.data.data.map(item => item.file.md.gif.url)
